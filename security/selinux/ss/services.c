5afc3792f98140af71c81de274748217da6be831
Jiapeng Chong <jiapeng.chong@linux.alibaba.com>
2021-04-28 18:00:15 +0800

    selinux: Remove redundant assignment to rc
    Variable rc is set to '-EINVAL' but this value is never read as
    it is overwritten or not used later on, hence it is a redundant
    assignment and can be removed.
    
    Cleans up the following clang-analyzer warning:
    
    security/selinux/ss/services.c:2103:3: warning: Value stored to 'rc' is
    never read [clang-analyzer-deadcode.DeadStores].
    
    security/selinux/ss/services.c:2079:2: warning: Value stored to 'rc' is
    never read [clang-analyzer-deadcode.DeadStores].
    
    security/selinux/ss/services.c:2071:2: warning: Value stored to 'rc' is
    never read [clang-analyzer-deadcode.DeadStores].
    
    security/selinux/ss/services.c:2062:2: warning: Value stored to 'rc' is
    never read [clang-analyzer-deadcode.DeadStores].
    
    security/selinux/ss/policydb.c:2592:3: warning: Value stored to 'rc' is
    never read [clang-analyzer-deadcode.DeadStores].
    
    Reported-by: Abaci Robot <abaci@linux.alibaba.com>
    Signed-off-by: Jiapeng Chong <jiapeng.chong@linux.alibaba.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -2115,7 +2115,6 @@ static int convert_context(struct context *oldc, struct context *newc, void *p,
 	context_init(newc);
 
 	/* Convert the user. */
-	rc = -EINVAL;
 	usrdatum = symtab_search(&args->newp->p_users,
 				 sym_name(args->oldp,
 					  SYM_USERS, oldc->user - 1));
@@ -2124,7 +2123,6 @@ static int convert_context(struct context *oldc, struct context *newc, void *p,
 	newc->user = usrdatum->value;
 
 	/* Convert the role. */
-	rc = -EINVAL;
 	role = symtab_search(&args->newp->p_roles,
 			     sym_name(args->oldp, SYM_ROLES, oldc->role - 1));
 	if (!role)
@@ -2132,7 +2130,6 @@ static int convert_context(struct context *oldc, struct context *newc, void *p,
 	newc->role = role->value;
 
 	/* Convert the type. */
-	rc = -EINVAL;
 	typdatum = symtab_search(&args->newp->p_types,
 				 sym_name(args->oldp,
 					  SYM_TYPES, oldc->type - 1));
@@ -2156,7 +2153,6 @@ static int convert_context(struct context *oldc, struct context *newc, void *p,
 		oc = args->newp->ocontexts[OCON_ISID];
 		while (oc && oc->sid[0] != SECINITSID_UNLABELED)
 			oc = oc->next;
-		rc = -EINVAL;
 		if (!oc) {
 			pr_err("SELinux:  unable to look up"
 				" the initial SIDs list\n");
